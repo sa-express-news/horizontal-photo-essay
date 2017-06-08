393e95a3afacc8bf6dc8a9bd81be95eab8bedc06
Kia Farhang <kfarhang0@gmail.com>
2017-06-08 14:31:44 -0500

    Created Sections component

---
+++ src/components/Sections/Sections.jsx
@@ -0,0 +1,34 @@
+// @flow
+
+import React, {Component} from 'react';
+
+import Section from '../Section/Section';
+
+class Sections extends Component {
+	props: {
+		sections: Object
+	};
+
+	render(){
+		const sections = this.props.sections;
+		console.log(typeof sections);
+		//Get rid of the leading array item, which is just a number key
+		const arrayOfSections = Object.entries(sections).map((data)=>{
+			return data[1];
+		});
+
+		console.log(typeof arrayOfSections);
+
+		const components = arrayOfSections.map((section, index) =>{
+			return <Section data={section} key={index}/>
+		});
+
+		return(
+			<div className='sections'>
+				{components}
+			</div>
+		)
+	}
+}
+
+export default Sections;

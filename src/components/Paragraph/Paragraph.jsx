e259fd8d6998326fc8f6c762809d787ed42d742f
Kia Farhang <kfarhang0@gmail.com>
2017-06-08 12:13:48 -0500

    Added Paragraph component

---
+++ src/components/Paragraph/Paragraph.jsx
@@ -0,0 +1,34 @@
+// @flow
+
+import React, {Component} from 'react';
+
+import './Paragraph.css';
+
+class Paragraph extends Component {
+	props: {
+		text: string,
+		isIntro: boolean
+	};
+
+	static defaultProps =  { 
+		isIntro: false
+	};
+
+	render(){
+		let graph = null;
+
+		if(this.props.isIntro){
+			graph = <p><span className='dropCap'>{this.props.text.substring(0,1)}</span>{this.props.text.slice(1)}</p>;
+		} else{
+			graph = <p>{this.props.text}</p>
+		}
+
+		return(
+			<div>
+			{graph}
+			</div>
+		)
+	}
+}
+
+export default Paragraph;

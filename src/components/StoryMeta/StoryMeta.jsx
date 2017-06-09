4868527eb58ff33de341a6973dfb19bc30891f47
Kia Farhang <kfarhang0@gmail.com>
2017-06-09 08:47:29 -0500

    Updated StoryMeta component to use headlines and subheads over generic headings

--- src/components/StoryMeta/StoryMeta.jsx
+++ src/components/StoryMeta/StoryMeta.jsx
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 
-import Heading from '../Heading/Heading';
-import Byline from '..Byline/Byline';
+import Headline from '../Headline/Headline';
+import Subhead from '../Subhead/Subhead';
+import Byline from '../Byline/Byline';
 import BylineDate from '../BylineDate/BylineDate';
 
 class StoryMeta extends Component{
@@ -12,8 +13,8 @@ class StoryMeta extends Component{
 	render(){
 		return(
 			<div className='StoryMeta'>
-				<Heading tag={2} text={this.props.meta.headline}/>
-				<Heading tag={5} text={this.props.meta.subhead}/>
+				<Headline text={this.props.meta.headline}/>
+				<Subhead text={this.props.meta.subhead}/>
 				<Byline text={this.props.meta.byline}/>
 				<BylineDate text={this.props.meta.date}/>
 			</div>

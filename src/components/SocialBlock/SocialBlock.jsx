5c0c4c9c210ba50b2c8ccf86e2fd60474497c276
Luke Whyte <lukeawhyte@gmail.com>
2017-11-01 17:22:50 -0500

    social link update

--- src/components/SocialBlock/SocialBlock.jsx
+++ src/components/SocialBlock/SocialBlock.jsx
@@ -9,8 +9,8 @@ import './SocialBlock.scss';
 export default () => (
   <div className="SocialBlock">
     <Social type='logo' link='//www.expressnews.com/' />
-    <Social type='facebook' link='//www.expressnews.com/caught-in-the-opioid-snare-san-antonio/' />
-    <Social type='twitter' link='//www.expressnews.com/caught-in-the-opioid-snare-san-antonio/' />
-    <Social type='reddit' link='//www.expressnews.com/caught-in-the-opioid-snare-san-antonio/' />
+    <Social type='facebook' link='http://www.expressnews.com/caught-in-the-opioid-snare-san-antonio/' />
+    <Social type='twitter' link='http://www.expressnews.com/caught-in-the-opioid-snare-san-antonio/' />
+    <Social type='reddit' link='http://www.expressnews.com/caught-in-the-opioid-snare-san-antonio/' />
   </div>
 ); 
